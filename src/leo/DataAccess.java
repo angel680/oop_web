28dce57f8ab143e22bf527c3edb4df3c2bb83eea
LiChenda <499268253@qq.com>
2015-11-11 12:15:43 +0800

    bug fixed

--- src/leo/DataAccess.java
+++ src/leo/DataAccess.java
@@ -37,12 +37,12 @@ public class DataAccess {
 		Student m_student = null;
 		
 		String sql = "SELECT userName, userPasswd, "
-				+ "userAuth, userEmail FROM students "
-				+ "WHERE userID = '" + userID +"'";
+					+ "userAuth, userEmail FROM students "
+					+ "WHERE userID = '" + userID +"'";
 		try {
 			ResultSet result = m_statement.executeQuery(sql);
-			boolean got = result.next();
-			if (got) {
+	
+			if (result.next()) {
 				String userName = result.getString(1);
 				String userPasswd = result.getString(2);
 				int userAuth = result.getInt(3);
@@ -53,18 +53,99 @@ public class DataAccess {
 				m_student = null;
 			}
 		} catch (SQLException e) {
-			System.out.println("LEO LOG: findStudent SQL EXECUTE FAIL");
-		}
-		
+			System.out.println("LEO LOG: findStudent SQL EXECUTE FAIL"
+					+ "\n\t" + e.getSQLState());
+		}		
 		return m_student;
 	}
 
+	public static Bulletin findBulletin(int bulletID){
+		Bulletin m_bulletin = null;
+		
+		String sql = "SELECT bulletMsg, bulletTime, "
+					+ "userID FROM bulletins "
+					+ "WHERE bulletID = " + String.valueOf(bulletID);
+		
+		try {
+			ResultSet result = m_statement.executeQuery(sql);
+			if (result.next()) {
+				String bulletMsg = result.getString(1);
+				String bulletTime = result.getString(2);
+				String userID = result.getString(3);
+				m_bulletin = new Bulletin(bulletID, 
+						bulletMsg, bulletTime, userID);
+			}else {
+				m_bulletin = null;
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("LEO LOG: findBulletin SQL EXECUTE FAIL"
+					+ "\n\t" + e.getSQLState());
+		}
+		
+		return m_bulletin;
+	}
+	
+	public static Comment findComment(int commentID){
+		Comment m_comment = null;
+		String sql = "SELECT commentMsg, commentTime, userID, "
+					+ "bulletID FROM comments "
+					+ "WHERE commentID = " + String.valueOf(commentID);
+		try {
+			ResultSet result = m_statement.executeQuery(sql);
+			if (result.next()) {
+				String commentMsg = result.getString(1);
+				String commentTime = result.getString(2);
+				String userID = result.getString(3);
+				int bulletID = result.getInt(4);
+				m_comment = new Comment(commentID, commentMsg, commentTime, userID, bulletID);
+			
+			}else {
+				m_comment = null;
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("LEO LOG: findComment SQL EXECUTE FAIL"
+					+ "\n\t" + e.getSQLState());
+		}
+		
+		return m_comment;
+	}
+	
+	public static Favor findFavor(int favorID){
+		Favor m_favor = null;
+		String sql = "SELECT favorTime, userID, bulletID "
+				+ "FROM favors "
+				+ "WHERE favorID = " + String.valueOf(favorID);
+		
+		try {
+			ResultSet result = m_statement.executeQuery(sql);
+			
+			if(result.next()){
+				String favorTime = result.getString(1);
+				String userID = result.getString(2);
+				int bulletID = result.getInt(3);
+				
+				m_favor = new Favor(favorID, favorTime, userID, bulletID);
+			}else {
+				m_favor = null;
+			}
+			
+			
+		} catch (SQLException e) {
+			System.out.println("LEO LOG: findFavor SQL EXECUTE FAIL"
+					+ "\n\t" + e.getSQLState());
+		}
+		
+		return m_favor;
+	}
+	
 	public static ArrayList<Comment> findCommentsByUser(String userID){
 		ArrayList<Comment> returnArray = new ArrayList<Comment>();
 		Comment m_comment;
 		
 		String sql = "SELECT commentID, commentMsg, "
-				+ "commentTime, bulletID FORM comments "
+				+ "commentTime, bulletID FROM comments "
 				+ "WHERE userID = '" + userID + "'";
 		try {
 			ResultSet result = m_statement.executeQuery(sql);
@@ -130,11 +211,88 @@ public class DataAccess {
 			}
 		} catch (SQLException e) {
 			System.out.println("LEO LOG: findFavorByUser SQL EXECUTE FAIL");			e.printStackTrace();
-		}	
-	
-		
+		}		
 		return returnArray;
 	}
 	
+	public static boolean addStudent(Student student){
+		String sql = "INSERT INTO students "
+				+ "(userID, userName, userPasswd, "
+				+ "userAuth, userEmail) "
+				+ "VALUES('" 
+					+ student.getUserID()+"', '"
+					+ student.getUserName() + "', '"
+					+ student.getUserPasswd() + "', "
+					+ String.valueOf(student.getUserAuth()) + ", '"
+					+ student.getUserEmail() 
+				+ "')";
+		try {
+			m_statement.executeUpdate(sql);
+			return true;
+		} catch (SQLException e) {
+			System.out.println("LEO LOG: addStudent SQL EXECUTE FAIL: \n"
+					+ "\t" + e.getSQLState());
+		}
+		return false;
+	}
+	
+	public static boolean addBulletin(Bulletin bulletin){
+		String sql = "INSERT INTO bulletins "
+				+ "(bulletID, bulletMsg, bulletTime, userID) "
+				+ "VALUES("
+					+ String.valueOf(bulletin.getBulletID())+ ", '"
+					+ bulletin.getBulletMsg() + "', '"
+					+ bulletin.getBulletTime() + "', '"
+					+ bulletin.getUserID() 
+				+ "')";
+		try {
+			m_statement.executeUpdate(sql);
+			return true;
+		} catch (SQLException e) {
+			System.out.println("LEO LOG: addBulletin SQL EXECUTE FAIL: \n"
+					+ "\t" + e.getSQLState());			
+		}
+		return false;
+	}
+	
+	public static boolean addComment(Comment comment){
+		String sql = "INSERT INTO comments "
+				+ "(commentID, commentMsg, "
+				+ "commentTime, userID, bulletID)"
+				+ "VALUES("
+					+ String.valueOf(comment.getCommentID()) + ", '"
+					+ comment.getCommentMsg() + "', '"
+					+ comment.getCommentTime() + "', '"
+					+ comment.getUserID() + "', "
+					+ String.valueOf(comment.getBulletID())
+				+ ")";
+		try {
+			m_statement.executeUpdate(sql);
+			return true;
+		} catch (SQLException e) {
+			System.out.println("LEO LOG: addComment SQL EXECUTE FAIL: \n"
+					+ "\t" + e.getSQLState());			
+		}
+		return false;
+	}
+	
+	public static boolean addFavor(Favor favor){
+		String sql = "INSERT INTO favors"
+				+ "(favorID, favorTime, userID, bulletID)"
+				+ "VALUES("
+					+ String.valueOf(favor.getFavorID()) + ", '"
+					+ favor.getFavorTime() + "', '"
+					+ favor.getUserID() + "', "
+					+ String.valueOf(favor.getBulletID())
+				+ ")";
+		try {
+			m_statement.executeUpdate(sql);
+			return true;
+		} catch (SQLException e) {
+			System.out.println("LEO LOG: addFavor SQL EXECUTE FAIL: \n"
+					+ "\t" + e.getSQLState());			
+		}
+		return false;
+	}
 	
 }
---
+++ src/leo/Tester.java
@@ -0,0 +1,11 @@
+package leo;
+import leo.datas.*;
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DataAccess.addStudent(new Student("U201417138",
+				"LI", "passwd", 1, "qq@qq.com"));
+	}
+
+}
